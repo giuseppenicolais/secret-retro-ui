affb335a9762268f282c167ea9c950d72b698f92
Brian McGrath <brian.mcgrath@clavistechnology.com>
2020-12-16 17:03:27 +0000

    Fixed width

--- src/LineChart.js
+++ src/LineChart.js
@@ -5,7 +5,7 @@ export default function LineChart({ data }) {
   return (
     <div>
       <Chart
-        width={'600px'}
+        // width={'600px'}
         height={'400px'}
         chartType="LineChart"
         loader={<div>Loading Chart</div>}
